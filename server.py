cf47cb773674f490c55bf72765526da530a6048d
richardLedbetter <cern4life@gmail.com>
2021-03-15 20:48:21 -0700

    working on auth

--- server.py
+++ server.py
@@ -1,8 +1,49 @@
 import db
 import time
 import flask
+from flask_login import LoginManager, login_required
 
 app = flask.Flask (__name__, static_folder="client/build", static_url_path="")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+#
+#Sample auth
+#
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@app.route("/settings")
+@login_required
+def settings():
+    pass
+
 
 ################################################################################
 #	Client Routes
